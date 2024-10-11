179d450bc28a234c728a897b27663c752f6926bf
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-10-11 14:12:19 -0500

    feat: home, shop and product detail screens added

--- app.py
+++ app.py
@@ -4,14 +4,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/product/<int:id>')
+def product(id):
+    return render_template('product.html', product_id=id)
+
+@app.route('/products/<category>')
+def products_by_category(category):
+    return render_template('products_category.html', category=category)
+
+@app.route('/products/brand/<brand>')
+def products_by_brand(brand):
+    return render_template('products_brand.html', brand=brand)
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/recommends')
+def recommends():
+    return render_template('recommends.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
